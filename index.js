11517f01c0b5a5075d796430f21031ebe475f6c8
ZhengJie <Doudou105@outlook.com>
2019-11-21 23:16:15 +0800

    feat: extend方法改进，添加 add() 数字相加方法

--- index.js
+++ index.js
@@ -26,7 +26,20 @@
 
         for (let name in options) {
 
-            target[name] = options[name].method;
+            var _methods = options[name].methods;
+            var _desc = options[name].desc;
+
+            for (let item in _methods) {
+
+                target[item] = _methods[item].method;
+
+                search_method_list.push({
+                    category: _desc,
+                    name: _methods[item].desc,
+                    method: item
+                })
+
+            }
 
         };
 
@@ -47,9 +60,62 @@
 
     // });
 
-    // NAITE.extend({
+    NAITE.extend({
 
-    // });
+        numberMethods: {
+            desc: '操作数字',
+            methods: {
+                add: {
+                    desc: '数字相加',
+                    method: function () {
+
+                        // 传参转化为数组
+                        var args = Array.prototype.slice.call(arguments);
+                        if (args.length == 0) {
+                            return 0
+                        };
+
+                        // 判断传入参数是否都为数字，否则抛出异常
+                        for (var i = 0; i < args.length; i++) {
+                            // 作为 0 处理
+                            if (args[i] == undefined || args[i] == null || args[i] == "") {
+                                args[i] = 0;
+                            };
+
+                            if (isNaN(args[i])) {
+                                throw 'arguments[' + (i) + '] is not number';
+                            };
+                        };
+
+                        // 获取小数点位数（10的倍数取值）
+                        var pointLength = 0;
+
+                        for (var i = 0; i < args.length.toString(); i++) {
+                            if (i == 0) {
+                                pointLength = args[i].toString().length;
+                            } else if (args[i].toString().length > pointLength) {
+                                pointLength = args[i].toString().length;
+                            }
+                        };
+
+                        args = args.map(item => item = 10 ** pointLength * item);
+
+                        // 计算结果
+                        var result = 0;
+
+                        for (var item of args) {
+                            result += item;
+                        };
+
+                        return result / (10 ** pointLength)
+
+                    }
+
+                }
+            }
+        }
+
+    });
 
     BOOT.J = NAITE;
 
